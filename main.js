bbcb41868deb9a8fc3b63d6e32de45d66f5e228c
fwoodmann <43883108+fwoodmann@users.noreply.github.com>
2021-04-15 00:28:23 +0200

    added basic web server feature

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+//Lesson 3 
 const cities = require("cities");
 var myCity = cities.zip_lookup("10016");
 console.log(myCity);
+
+//Lesson 4
+const port = 3000,
+http = require("http"),
+httpStatus = require("http-status-codes"),
+app = http.createServer((request,response) => {
+  console.log("Received an incoming request!");
+  response.writeHead(httpStatus.OK,{
+    "Content-Type": "text/html"
+  });
+  let responseMessage = "<h1>Hello, Universe!</h1>";
+  response.write(responseMessage);
+  response.end();
+  console.log(`Sent a response : ${responseMessage}`);
+});
+app.listen(port);
+console.log(`This server has started and is listening in port number: ${port}`);
