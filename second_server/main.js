b56095f37e4efa7e06bec40cb37965a465ec979e
fwoodmann <43883108+fwoodmann@users.noreply.github.com>
2021-04-15 00:58:30 +0200

    added routes

--- second_server/main.js
+++ second_server/main.js
@@ -1,3 +1,11 @@
+const routeResponseMap = {
+  "/info": "<h1>Info Page</h1>",
+  "/contact": "<h1>Contact Us</h1>",
+  "/about": "<h1>Learn More About Us.</h1>",
+  "/hello": "<h1>Say hello by emailing us here</h1>",
+  "/error": "<h1>Sorry the page you are looking for is not here.</h1>"
+}
+
 const port = 3000,
 http = require("http"),
 httpStatus = require("http-status-codes"),
@@ -25,8 +33,11 @@ console.log(`Headers: ${getJSONString(req.headers)}`);
 res.writeHead(httpStatus.OK,{
   "Content-Type": "text/html"
 });
-let responseMessage = "<h1> This will show on the screen. </h1>"
-res.end(responseMessage);
+if (routeResponseMap[req.url]) {
+  setTimeout(() => res.end(routeResponseMap[req.url]), 2000);
+  } else {
+  res.end("<h1>Welcome!</h1>");
+  }
 });
 app.listen(port);
 console.log(`This server has started and is listening in port number: ${port}`);
