ba76624465a7098f82e3c52215b9027023e80e0a
fwoodmann <43883108+fwoodmann@users.noreply.github.com>
2021-04-15 20:46:58 +0200

    web server ready to configure

---
+++ confetti_cuisine/main.js
@@ -0,0 +1,14 @@
+const port = 3000,
+  http = require("http"),
+  httpStatus = require("http-status-codes"),
+  router = require("./router"),
+  contentTypes = require("./contentTypes"),
+  utils = require("./utils");
+
+  router.get("/", (req, res) => {
+    res.writeHead(httpStatus.OK, contentTypes.html);
+    utils.getFile("views/index.html", res);
+  });
+  
+  http.createServer(router.handle).listen(port);
+  console.log(`The server is listening on port number: http://localhost:${port}`);
