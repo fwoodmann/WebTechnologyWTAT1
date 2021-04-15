bf40395e71038164787c7eed30323064b68377f0
fwoodmann <43883108+fwoodmann@users.noreply.github.com>
2021-04-15 20:17:36 +0200

    added router function

--- serve_html/main.js
+++ serve_html/main.js
@@ -1,23 +1,33 @@
 const port = 3000,
   http = require("http"),
   httpStatus = require("http-status-codes"),
-  fs = require("fs");
-const routeMap = {
-  "/": "views/index.html"
-};
-http
-  .createServer((req, res) => {
-    res.writeHead(httpStatus.OK, {
-      "Content-Type": "text/html"
+  fs = require("fs"),
+  router = require("./router");
+plainTextContentType = {
+    "Content-Type": "text/plain"
+  },
+  htmlContentType = {
+    "Content-Type": "text/html"
+  },
+  customReadFile = (file, res) => {
+    fs.readFile(`./${file}`, (errors, data) => {
+      if (errors) {
+        console.log("Error reading the file...");
+      }
+      res.end(data);
     });
-    if (routeMap[req.url]) {
-      fs.readFile(routeMap[req.url], (error, data) => {
-        res.write(data);
-        res.end();
-      });
-    } else {
-      res.end("<h1>Sorry, not found.</h1>");
-    }
-  })
-  .listen(port);
+  };
+router.get("/", (req, res) => {
+  res.writeHead(httpStatus.OK, plainTextContentType);
+  res.end("INDEX");
+});
+router.get("/index.html", (req, res) => {
+  res.writeHead(httpStatus.OK, htmlContentType);
+  customReadFile("views/index.html", res);
+});
+router.post("/", (req, res) => {
+  res.writeHead(httpStatus.OK, plainTextContentType);
+  res.end("POSTED");
+});
+http.createServer(router.handle).listen(3000);
 console.log(`The server has started and is listening on port number: ${port}`);
