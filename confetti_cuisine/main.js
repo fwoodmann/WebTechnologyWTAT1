0c0b662f18a0a111c75ff2bc049edcdc65aef640
fwoodmann <43883108+fwoodmann@users.noreply.github.com>
2021-04-16 20:42:52 +0200

    added picture to website

--- confetti_cuisine/main.js
+++ confetti_cuisine/main.js
@@ -4,11 +4,17 @@ const port = 3000,
   router = require("./router"),
   contentTypes = require("./contentTypes"),
   utils = require("./utils");
-
+  var pictureName = "nice.jpg";
   router.get("/", (req, res) => {
     res.writeHead(httpStatus.OK, contentTypes.html);
     utils.getFile("views/index.html", res);
+    //res.writeHead(httpStatus.OK, contentTypes.png);
+   // utils.getFile("public/images/35227bc0-179e-41f8-872f-56eb08a30f4d.jpg",res);
   });
+  router.get("/"+ pictureName,(req,res) =>{
+    res.writeHead(httpStatus.OK, contentTypes.png);
+    utils.getFile("public/images/" + req.url,res);
+  })
   
   http.createServer(router.handle).listen(port);
   console.log(`The server is listening on port number: http://localhost:${port}`);
